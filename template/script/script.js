d1c890c0ca10e2de63aab5c082d845bcf0710df1
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-07 12:48:26 +0100

    refactor groups page

--- template/script/script.js
+++ template/script/script.js
@@ -1,4 +1,6 @@
 const addUser = () => {
   let addUserForm = document.querySelector('.add-user-form');
+  let addUserTrigger = document.querySelector('.add-user-trigger')
   addUserForm.classList.toggle('display-block');
+  addUserTrigger.classList.toggle('display-none');
 }
