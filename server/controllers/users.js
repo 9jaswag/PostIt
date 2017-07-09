427308c8cd6509a567bd7aee2989dbe112d2868a
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-09 20:29:23 +0100

    add user controller

---
+++ server/controllers/users.js
@@ -0,0 +1,19 @@
+/**
+ * User controller
+ * handles every user related task
+ */
+
+const User = require('../models').User;
+
+module.exports = {
+  signup (req, res) {
+    return User
+      .create({
+        username:req.body.username,
+        password: req.body.password,
+        email: req.body.password
+      })
+      .then(user => res.status(201).send(user))
+      .catch(error => res.status(400).send(error));
+  }
+}
