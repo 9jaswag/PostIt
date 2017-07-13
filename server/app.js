a103fcdce5f6e8207e5d7ceac7638f61ccf63343
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-13 12:55:26 +0100

    refactor app.js to es6 standard

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const logger = require('morgan');
+// const bodyParser = require('body-parser');
+// require('dotenv').config();
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
 require('dotenv').config();
 
 // Set up the express app
