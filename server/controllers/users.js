751fbda5dac5c1fe0ba5ad3f26292cd1a2295bd9
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-18 12:15:47 +0100

    refactor user controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -9,23 +9,54 @@ import models from '../models';
 
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
+const sequelizeErrors = {};
 
 export default {
   signup(req, res) {
-    if (!req.body.username) {
-      return res.status(401)
-        .send({ status: false, message: 'Please choose a username' });
-    } else if (!req.body.password) {
-      return res.status(401)
-        .send({ status: false, message: 'Please choose a password' });
+    // validation checks
+    if (!req.body.username || req.body.username.trim() === '') {
+      return res.status(400)
+        .send({
+          error: {
+            status: false, message: 'Username field cannot be empty'
+          },
+          data: req.body
+        });
+    } else if (!req.body.password || req.body.password.trim() === '') {
+      return res.status(400)
+        .send({
+          error: {
+            status: false, message: 'Password field cannot be empty'
+          },
+          data: req.body
+        });
     } else if (req.body.password.length < 6) {
       return res.status(400)
-        .send({ status: false,
-          message: 'Password length must be more than 6 characters' });
-    } else if (!req.body.email) {
+        .send({
+          error: {
+            status: false,
+            message: 'Password length must be more than 6 characters'
+          },
+          data: req.body
+        });
+    } else if (!req.body.email || req.body.email.trim() === '') {
       return res.status(400)
-        .send({ status: false, message: 'Please enter an email address' });
+        .send({
+          error: {
+            status: false, message: 'Email address field cannot be empty'
+          },
+          data: req.body
+        });
+    } else if (!req.body.phone || req.body.phone.trim() === '') {
+      return res.status(400)
+        .send({
+          error: {
+            status: false, message: 'Phone field cannot be empty'
+          },
+          data: req.body
+        });
     }
+    // check if email already exists
     models.User.findOne({
       where: {
         email: req.body.email,
@@ -33,14 +64,18 @@ export default {
     }).then((user) => {
       if (user) {
         return res.status(400)
-          .send({ status: false, message: 'Email address already exists' });
+          .send({
+            error: { status: false, message: 'Email address already exists' },
+            data: req.body
+          });
       }
     });
     return models.User
       .create({
-        username: req.body.username,
+        username: req.body.username.trim(),
         password: bcrypt.hashSync(req.body.password, salt),
-        email: req.body.email
+        email: req.body.email.trim(),
+        phone: req.body.phone.trim()
       })
       .then((user) => {
         const token = jwt
@@ -48,14 +83,30 @@ export default {
             userId: user.id,
             userEmail: user.email,
             userUsername: user.username,
-          }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
+            userPhone: user.phone
+          }, process.env.TOKEN_SECRET, { expiresIn: '2h' });
         res.status(201)
-          .send({ success: true, message: 'Sign up succesful.', token });
+          .send({ success: true,
+            message: 'Sign up succesful.',
+            data: { token } });
       })
-      .catch(error => res.status(400).send({
-        status: false,
-        message: error.message
-      }));
+      .catch((error) => {
+        if (error.errors[0].message === 'Validation is on username failed') {
+          sequelizeErrors.error = 'Username cannot contain numbers';
+        }
+        if (error.errors[0].message === 'Email can not be empty') {
+          sequelizeErrors.error = 'Email field can not be empty';
+        }
+        if (error.errors[0].message === 'Enter a valid email address') {
+          sequelizeErrors.error = 'Email address is invalid';
+        }
+        if (error.errors[0].message === 'Validation notEmpty on phone failed') {
+          sequelizeErrors.error = 'Phone field can not be empty';
+        }
+        sequelizeErrors.data = req.body;
+        res.status(400).send({ status: false, errors: sequelizeErrors });
+      }
+      );
   },
   login(req, res) {
     if (!req.body.username) {
