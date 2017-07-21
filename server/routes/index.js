1ac35232367e05b909bca9b6d6632d417f81f1a5
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-21 15:15:45 +0100

    restructured routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,43 +1,33 @@
 import jwt from 'jsonwebtoken';
 import controllers from '../controllers';
+import path from 'path';
+import auth from '../middlewares/auth';
+
 
 module.exports = (app) => {
   // base API
-  app.get('/', (req, res) => res.status(200).send({
+  app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Post IT API!',
   }));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/index.html'));
+  });
 
   // API route to handle user sign up
   app.post('/api/user/signup', controllers.users.signup);
   // API route to handle user sign in
   app.post('/api/user/signin', controllers.users.login);
-
-  // Middleware
-  let token;
-  app.use((req, res, next) => {
-    token = req.headers['x-access-token'];
-    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-        return res.status(401).send({
-          message: 'User not authenticated. Failed to authenticate token.'
-        });
-      }
-      // if everything is good, save to request for use in other routes
-      req.decoded = decoded;
-      next();
-    });
-  });
+  // // API to get all users
+  app.get('/api/users', auth, controllers.users.findAll);
+  // API for logged in users to retrieve messages in their group
+  app.get('/api/group/:group_id/messages', auth, controllers.groups.fetchMessage);
 
   // API to create new group
-  app.post('/api/group', controllers.groups.create);
+  app.post('/api/group', auth, controllers.groups.create);
   // API route for users to add other users to groups:
-  app.post('/api/group/:group_id/user', controllers.groups.addUser);
-  // API to get all users
-  // app.get('/api/users', controllers.users.findAll);
-  app.get('/api/users', controllers.users.findAll);
+  app.post('/api/group/:group_id/user', auth, controllers.groups.addUser);
 
   // API for logged in users to post messages to a group
-  app.post('/api/group/:group_id/message', controllers.groups.postMessage);
-  // API for logged in users to retrieve messages in their group
-  app.get('/api/group/:group_id/messages', controllers.groups.fetchMessage);
+  app.post('/api/group/:group_id/message', auth, controllers.groups.postMessage);
 };
