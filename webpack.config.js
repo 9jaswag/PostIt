a3664de1f60a6e780db091a457d891999b7eb8e4
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-21 16:48:55 +0100

    modify webpack  config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+/**
+ * Webpach config
+ */
+
+ import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+  entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client?reload=true', // note that it reloads the page if hot module reloading fails.
+    path.resolve(__dirname, 'client/index.js')
+  ],
+  target: 'web',
+  output: {
+    path: `${__dirname}/client/dist`, // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'client')
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, include: path.join(__dirname, 'client'), loaders: ['babel-loader'] },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /(\.css)$/, loaders: ['style-loader', 'css-loader'] },
+      { test: /\.scss$/, loader: 'style-loader!css-loader?url=false!sass-loader' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+    ]
+  }
+};
