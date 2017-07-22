f19852a7a54516eb947596e8f48f26675f796420
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-22 12:46:56 +0100

    modify webpack config ans express app server for webpack bundling

--- server/app.js
+++ server/app.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import webpack from 'webpack';
+import config from '../webpack.config';
 
 require('dotenv').config();
 
 // Set up the express app
 const app = express();
+const compiler = webpack(config);
 
 // Log requests to the console.
 app.use(logger('dev'));
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,41 @@
 /**
- * Webpach config
+ * Webpack config
  */
 
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  // debug: true,
+  devtool: 'inline-source-map',
+  // noInfo: false,
+  entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client?reload=true', // note that it reloads the page if hot module reloading fails.
+    path.resolve(__dirname, 'client/index')
+  ],
+  target: 'web',
+  output: {
+    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'client')
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel-loader'] },
+      { test: /\.scss$/, loader: 'style-loader!css-loader?url=false!sass-loader' },
+      { test: /(\.css)$/, loaders: ['style-loader', 'css-loader'] },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+    ]
+  }
+};
