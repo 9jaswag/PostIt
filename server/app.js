13b0fd36cea176a4a24eb0adca32ae7eaa5604a9
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-23 20:28:50 +0100

    modify express server

--- server/app.js
+++ server/app.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import webpack from 'webpack';
+import config from '../webpack.config';
 
 require('dotenv').config();
 
 // Set up the express app
 const app = express();
+const compiler = webpack(config);
 
 // Log requests to the console.
 app.use(logger('dev'));
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+app.use(require('webpack-hot-middleware')(compiler));
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
