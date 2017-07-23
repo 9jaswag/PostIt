de6a7c1f6d142bc09ee6ece810283d607b9a23da
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-23 21:55:35 +0100

    modify index.js file

--- client/index.js
+++ client/index.js
@@ -2,8 +2,8 @@ import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-import appRoutes from './components/appRoutes';
-import App from './components/App';
+// import appRoutes from './components/appRoutes';
+// import App from './components/App';
 import HomePage from './components/home/HomePage';
 import './styles/main.scss';
 
