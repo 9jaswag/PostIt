ae4af89ae868e7a5322669129098024656c7b084
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-23 21:47:12 +0100

    modify client index.js file

--- client/index.js
+++ client/index.js
@@ -1,1 +1,11 @@
-console.log('hi');
+import 'babel-polyfill'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import appRoutes from './components/appRoutes';
+import App from './components/App';
+import HomePage from './components/home/HomePage';
+import './styles/main.scss';
+
+
+ReactDOM.render(<HomePage />, document.getElementById('app'));
