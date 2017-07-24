541d39f1f6e8b8509d5c7b8c97f61a3f5b0a9522
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-24 15:18:23 +0100

    Create homepage component

--- client/components/home/HomePage.js
+++ client/components/home/HomePage.js
@@ -3,7 +3,8 @@ import React from 'react';
 const HomePage = (props) => {
   return (
     <div>
-      <div id="signupModal">
+      {/* Sign Up Modal */}
+      <div id="signupModal" className="modal">
         <div className="modal-content">
           <h4>Sign Up</h4>
           <form action="" className="col s12">
@@ -13,8 +14,8 @@ const HomePage = (props) => {
                 <label htmlFor="username">Username</label>
               </div>
               <div className="input-field col s6">
-                <input id="password" type="text" className="validate" required />
-                <label htmlFor="password">Password</label>
+                <input id="username" type="text" className="validate" required />
+                <label htmlFor="username">Username</label>
               </div>
             </div>
             <div className="row">
@@ -35,6 +36,7 @@ const HomePage = (props) => {
           </form>
         </div>
       </div>
+      {/* Sign In Modal */}
       <div id="signinModal" className="modal">
         <div className="modal-content">
           <h4>Sign In</h4>
@@ -59,22 +61,26 @@ const HomePage = (props) => {
           </form>
         </div>
       </div>
-      <div className="full-height fh">
-        <div className="row">
-          <div className="col s12 m6 l6 teal accent-4 valign-wrapper full-height">
-            <div style={{ display: 'block', margin: 'auto' }}>
-              <h2 className="center-align text-white">PostIT</h2>
-              <h2 className="center-align text-white">Prompt Messages, Prompt Delivery</h2>
+      { /* Page Content */}
+      <div>
+        <div className="full-height fh">
+          <div className="row">
+            <div className="col s12 m6 l6 teal accent-4 valign-wrapper full-height">
+              <div style={{ display: 'block', margin: 'auto' }}>
+                <h2 className="center-align text-white">PostIT</h2>
+                <h5 className="center-align text-white">Prompt Messages, Prompt Delivery</h5>
+                { /* Modal Buttons for mobile only*/ }
+                <div className="show-on-small hide-on-med-and-up center-align margin-v2">
+                  <a href="#signupModal" className="waves-effect waves-light btn modal-trigger">Sign Up</a>
+                  <a href="#signinModal" className="waves-effect waves-light btn modal-trigger margin-h">Sign In</a>
+                </div>
+              </div> 
             </div>
-            <div className="show-on-small hide-on-med-and-up center-align margin-v2">
-              <a href="#signupModal" className="waves-effect waves-light btn modal-trigger">Sign Up</a>
-              <a href="#signinModal" className="waves-effect waves-light btn modal-trigger">Sign In</a>
-            </div>
-          </div>
-          <div className="col s12 m6 l6 valign-wrapper full-height hide-on-small-only">
-            <div className="center-align" style={{ display: 'block', margin: 'auto' }}>
-              <a href="#signupModal" className="waves-effect waves-light btn modal-trigger">Sign Up</a>
-              <a href="#signinModal" className="waves-effect waves-light btn modal-trigger">Sign In</a>
+            <div className="col s12 m6 l6 valign-wrapper full-height hide-on-small-only">
+              <div className="center-align" style={{ display: 'block', margin: 'auto' }}>
+                <a className="waves-effect waves-light btn modal-trigger" href="#signupModal">Sign Up</a>
+                <a className="waves-effect waves-light btn modal-trigger margin-h" href="#signinModal">Sign In</a>
+              </div>
             </div>
           </div>
         </div>
