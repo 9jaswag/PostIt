d1263dc6e07f939b6ec187b68d8fc41f0232213b
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-26 22:22:59 +0100

    modify App component

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, browserHistory } from 'react-router-dom';
 import HomePage from './home/HomePage';
+import DashboardPage from './dashboard/DashboardPage';
 import GroupPage from './group/GroupPage';
 
 class App extends Component {
   render() {
     return(
-      <GroupPage />
+        <BrowserRouter history={ browserHistory }>
+          <div>
+            <Route exact path="/" component={ HomePage } />
+            <Route path="/dashboard" component={ DashboardPage} />
+            <Route path="/group" component={ GroupPage } />
+          </div>
+        </BrowserRouter>
     );
   }
 }
