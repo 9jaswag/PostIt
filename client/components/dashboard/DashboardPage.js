b8956056e7130fb98dbda403a0c1924ede5d4e1b
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-26 22:29:58 +0100

    modify dashboard component

--- client/components/dashboard/DashboardPage.js
+++ client/components/dashboard/DashboardPage.js
@@ -13,7 +13,7 @@ class DashboardPage extends Component {
             <div className="col s12 m12 l12">
               <h5 className="center-align uppercase" style={{ marginBottom: '2rem' }}>My Groups</h5>
               { /*Group cards*/ }
-              <a href="" className="tooltipped pointer"  data-position="right" data-delay="50" data-tooltip="This is a short group description of a maximum of about 70 chars.">
+              <a href="/group" className="tooltipped pointer"  data-position="right" data-delay="50" data-tooltip="This is a short group description of a maximum of about 70 chars.">
                 <div className="col s12 m6 l4">
                   <div className="card-panel hoverable">Andela Bootcamp <span className="new badge">4</span></div>
                 </div>
