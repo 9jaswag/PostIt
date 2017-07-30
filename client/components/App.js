592353341eb3e7f65323a04244062bb03d96e92f
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-30 21:32:59 +0100

    Add routing

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './home/HomePage';
+import DashboardPage from './dashboard/DashboardPage'
 import GroupPage from './group/GroupPage';
 
 class App extends Component {
   render() {
     return(
-      <GroupPage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/dashboard" component={DashboardPage} />
+          <Route exact path="/group" component={GroupPage} />
+          <Route render={() => {
+            return <p>404 Not Found!</p>;
+          }} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-// import appRoutes from './components/appRoutes';
 import App from './components/App';
 import './styles/main.scss';
 
