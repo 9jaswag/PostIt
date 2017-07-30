3cf83b82280c8d37cc4b9fdda1229a76ee91446f
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-30 21:42:52 +0100

    modify user and group models

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -9,50 +9,33 @@ import models from '../models';
 
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
-const sequelizeErrors = {};
 
 export default {
   signup(req, res) {
+    const errors = { };
+    let hasError = false;
     // validation checks
     if (!req.body.username || req.body.username.trim() === '') {
-      return res.status(400)
-        .send({
-          status: false,
-          error: { message: 'Username field cannot be empty' },
-          data: req.body
-        });
-    } else if (!req.body.password || req.body.password.trim() === '') {
-      return res.status(400)
-        .send({
-          status: false,
-          error: { message: 'Password field cannot be empty' },
-          data: req.body
-        });
+      hasError = true;
+      errors.username = 'Username field cannot be empty';
+    } if (!req.body.password || req.body.password.trim() === '') {
+      hasError = true;
+      errors.password = 'Password field cannot be empty';
     } else if (req.body.password.length < 6) {
-      return res.status(400)
-        .send({
-          status: false,
-          error: { message: 'Password length must be more than 6 characters' },
-          data: req.body
-        });
-    } else if (!req.body.email || req.body.email.trim() === '') {
-      return res.status(400)
-        .send({
-          status: false,
-          error: {
-            message: 'Email address field cannot be empty'
-          },
-          data: req.body
-        });
-    } else if (!req.body.phone || req.body.phone.trim() === '') {
-      return res.status(400)
-        .send({
-          status: false,
-          error: {
-            message: 'Phone field cannot be empty'
-          },
-          data: req.body
-        });
+      hasError = true;
+      errors.password = 'Password length must be more than 6 characters';
+    } if (!req.body.email || req.body.email.trim() === '') {
+      hasError = true;
+      errors.email = 'Email address field cannot be empty';
+    } if (!req.body.phone || req.body.phone.trim() === '') {
+      hasError = true;
+      errors.phone = 'Phone field cannot be empty';
+    } else if (isNaN(req.body.phone)) {
+      hasError = true;
+      errors.phone = 'Phone number cannot contain text';
+    }
+    if (hasError) {
+      return res.status(400).send({ success: false, errors });
     }
     // check if email already exists
     models.User.findOne({
@@ -61,17 +44,18 @@ export default {
       }
     }).then((user) => {
       if (user) {
+        hasError = true;
+        errors.email = 'Email address already exists';
         return res.status(400)
           .send({
-            status: false,
-            error: { message: 'Email address already exists' },
-            data: req.body
+            success: false,
+            errors
           });
       }
     });
     return models.User
       .create({
-        username: req.body.username.trim(),
+        username: req.body.username.trim().toLowerCase(),
         password: bcrypt.hashSync(req.body.password, salt),
         email: req.body.email.trim(),
         phone: req.body.phone.trim()
@@ -90,32 +74,43 @@ export default {
             data: { token } });
       })
       .catch((error) => {
-        if (error.errors[0].message === 'Validation is on username failed') {
-          sequelizeErrors.error = 'Username cannot contain numbers';
-        }
         if (error.errors[0].message === 'Username already exists') {
-          sequelizeErrors.error = 'Username already exists';
+          hasError = true;
+          errors.username = 'Username already exists';
         }
         if (error.errors[0].message === 'Email can not be empty') {
-          sequelizeErrors.error = 'Email field can not be empty';
+          hasError = true;
+          errors.email = 'Email field can not be empty';
         }
         if (error.errors[0].message === 'Enter a valid email address') {
-          sequelizeErrors.error = 'Email address is invalid';
+          hasError = true;
+          errors.email = 'Email address is invalid';
         }
         if (error.errors[0].message === 'Validation notEmpty on phone failed') {
-          sequelizeErrors.error = 'Phone field can not be empty';
+          hasError = true;
+          errors.phone = 'Phone field can not be empty';
         }
-        sequelizeErrors.data = req.body;
-        res.status(400).send({ status: false, errors: sequelizeErrors });
+        if (error.errors[0].message === 'Phone number already exists') {
+          hasError = true;
+          errors.phone = 'Phone number already exists';
+        }
+        if (error.errors[0].message === 'Phone number must have 11 characters') {
+          hasError = true;
+          errors.phone = 'Phone number must have 11 characters';
+        }
+        if (error.errors[0].message === 'Only numeric characters are allowed as phone numbers') {
+          hasError = true;
+          errors.phone = 'Only numeric characters are allowed as phone numbers';
+        }
+        res.status(400).send({ success: false, errors });
       }
-      // error => res.status(400).send(error)
       );
   },
   login(req, res) {
     if (!req.body.username) {
       return res.status(401)
         .send({
-          status: false,
+          success: false,
           error: {
             message: 'Username field cannot be empty'
           },
@@ -124,7 +119,7 @@ export default {
     } else if (!req.body.password) {
       return res.status(401)
         .send({
-          status: false,
+          success: false,
           error: {
             message: 'Password field cannot be empty'
           },
@@ -139,7 +134,7 @@ export default {
       if (!user) {
         return res.status(401)
           .send({
-            status: false,
+            success: false,
             error: {
               message: 'User does not exist'
             }
@@ -149,7 +144,7 @@ export default {
         if (!(bcrypt.compareSync(req.body.password, passwordHash))) {
           return res.status(401)
             .send({
-              status: false,
+              success: false,
               error: {
                 message: 'Incorrect password!'
               }
@@ -168,7 +163,7 @@ export default {
           data: { token } });
     })
       .catch(error => res.status(400).send({
-        status: false,
+        success: false,
         error: { message: error.message }
       }));
   },
@@ -178,12 +173,12 @@ export default {
       .then((user) => {
         if (user.length === 0) {
           return res.status(200)
-            .send({ status: true, message: 'No users found' });
+            .send({ success: true, message: 'No users found' });
         }
-        return res.status(200).json({ data: { user } });
+        return res.status(200).send({ data: { user } });
       })
       .catch(error => res.status(400).send({
-        status: false,
+        success: false,
         error: { message: error.message }
       }));
   }
