b89daba78f91d20c6be1522f6d259aaf9a7456f0
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-31 23:14:47 +0100

    add jwt to localstorage for sign in

--- client/components/actions/signinAction.js
+++ client/components/actions/signinAction.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 const Login = userData =>
-  (dispatch) => axios.post('/api/user/signin', userData);
+  (dispatch) => axios.post('/api/user/signin', userData).then((response) => {
+    const token = response.data.data.token;
+    localStorage.setItem('jwtToken', token);
+  });
 
 export default Login;
