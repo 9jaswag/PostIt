f69e05bb21a0783a4347570cd44bf995cdc2523d
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-31 22:51:52 +0100

    modify login controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -121,7 +121,7 @@ export default {
     }
     return models.User.findOne({
       where: {
-        username: req.body.username,
+        username: req.body.username.toLowerCase(),
       }
     }).then((user) => {
       if (!user) {
