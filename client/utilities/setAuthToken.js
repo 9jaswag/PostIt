b7b2794b363b3f5dbfc4399b3cc98d51be2e1488
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-01 10:28:16 +0100

    add js doc

--- client/utilities/setAuthToken.js
+++ client/utilities/setAuthToken.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+/**
+ * Function for setting auth token in header
+ * @param {string} token 
+ */
 export default function setAuthToken(token) {
   if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
