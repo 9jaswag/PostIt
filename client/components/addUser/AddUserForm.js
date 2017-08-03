83f9f1124dc45722e70f16b9d472b1900051790b
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-04 00:47:30 +0100

    add user to group form and state

--- client/components/addUser/AddUserForm.js
+++ client/components/addUser/AddUserForm.js
@@ -7,7 +7,22 @@ import React, { Component } from 'react';
 class AddUserForm extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      users: [],
+      username: ''
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e){
+    e.preventDefault();
+    console.log(this.state)
+  }
+
   render() {
     return(
       <div>
@@ -15,11 +30,11 @@ class AddUserForm extends Component {
         <div id="addUserModal" className="modal">
           <div className="modal-content">
             <h4>Add New User To Group</h4>
-            <form action="" className="col s12">
+            <form action="" className="col s12" onSubmit={ this.onSubmit }>
               <div className="row">
                 <div className="input-field col s12">
-                  <input id="username" type="text" className="validate" required/>
-                  <label htmlFor="first_name">Enter username</label>
+                  <input id="username" name="username" type="text" className="validate" value={ this.state.username } onChange= { this.onChange} required/>
+                  <label htmlFor="username">Enter username</label>
                 </div>
               </div>
               <div className="row">
