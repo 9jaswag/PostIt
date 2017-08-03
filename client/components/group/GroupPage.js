1c44164a48d3d98732c178708c710070ccd872dd
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-03 16:45:42 +0100

    remove adduser and post message form from group page component

--- client/components/group/GroupPage.js
+++ client/components/group/GroupPage.js
@@ -8,34 +8,12 @@ class GroupPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showMessageDiv: false,
-      showAddUserDiv: false,
-      messages: []
+      messages: [],
+      message: '',
+      priority: ''
     };
-
-    this.postMessage = this.postMessage.bind(this);
-    this.addUser = this.addUser.bind(this);
+    
     this.onLoad = this.onLoad.bind(this);
-  }
-  postMessage() {
-    if (this.state.showAddUserDiv) {
-      this.setState(prevState => ({
-        showAddUserDiv: !prevState.showAddUserDiv
-      }));
-    }
-    this.setState(prevState => ({
-      showMessageDiv: !prevState.showMessageDiv
-    }));
-  }
-  addUser() {
-    if (this.state.showMessageDiv) {
-      this.setState(prevState => ({
-        showMessageDiv: !prevState.showMessageDiv
-      })); 
-    }
-    this.setState(prevState => ({
-      showAddUserDiv: !prevState.showAddUserDiv
-    }));
   }
   onLoad() {
     this.props.getMessages(this.props.groupDetails.split(' ')[0]).then(
@@ -95,13 +73,13 @@ class GroupPage extends Component {
                 </div>
                 { /*Send A Message div*/ }
                 <div className="col s12 m12 l12 no-padding">
-                  { this.state.showMessageDiv ? <MessageDiv onClick={ this.postMessage } /> : null }
+                  <MessageDiv />
                   <button className="btn margin-v2 post-message-toggle one-whole" onClick={ this.postMessage }>Send A Message</button>
                 </div>
                 <hr/>
                 { /*Add new user div*/ }
                 <div className="col s12 m12 l12 no-padding">
-                  { this.state.showAddUserDiv ? <AddUserdiv onClick={ this.addUser }/> : null }
+                  <AddUserdiv />
                   <button className="btn margin-v2 add-user-trigger one-whole" onClick={ this.addUser }>Add New Users To Group</button>
                 </div>
               </div>
@@ -111,60 +89,6 @@ class GroupPage extends Component {
       </div>
     );
   }
-}
-
-const MessageDiv = (props) => {
-  return(
-    <section className="post-message margin-v2">
-      <h6 className="uppercase center-align">Post a new message</h6>
-      <form action="" className="col s12">
-        <div className="row">
-          <div className="input-field col s12">
-            <textarea cols="30" rows="10" id="message" className="materialize-textarea" required></textarea>
-            <label htmlFor="message">Message</label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s12">
-            <select name="priority" id="priority">
-              <option value="normal" defaultValue>Normal</option>
-              <option value="urgent">Urgent</option>
-              <option value="critical">Critical</option>
-            </select>
-            <label htmlFor="priority">Message Priority</label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s12">
-            <input className="btn one-whole" type="submit" value="Send Message"/>
-            <input onClick={ props.onClick } className="btn one-whole post-message-cancel" type="reset" value="Cancel" style={{ marginTop: '.5rem' }}/>
-          </div>
-        </div>
-      </form>
-    </section>
-  );
-}
-
-const AddUserdiv = (props) => {
-  return(
-    <section className="add-user-form">
-      <h6 className="center-align uppercase">Search for a user and add them to this group</h6>
-      <form action="" className="col s12">
-        <div className="row">
-          <div className="input-field col s12">
-            <input id="username" type="text" className="validate" required/>
-            <label htmlFor="first_name">Enter username</label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s12">
-            <input className="btn one-whole" type="submit" value="Add User"/>
-            <input onClick={ props.onClick } className="btn one-whole adduser-cancel" type="reset" value="Cancel" style={{ marginTop: '.5rem' }}/>
-          </div>
-        </div>
-      </form>
-    </section>
-  );
 }
 
 GroupPage.propTypes = {
