982ee876cd139ca1b54dd81c8fddbfc6011c1f45
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-05 18:56:16 +0100

    searchUser Action

---
+++ client/actions/searchUserAction.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const searchUserAction = (searchTerm) =>
+  // (dispatch) => axios.post(`/api/group/${id}/message`, searchTerm);
+  console.log(searchTerm);
+
+export default searchUserAction;
