8d01ffc420eaab315f05bb344c3a9d27f282802d
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-05 18:55:27 +0100

    searchpage state

--- client/components/search/Searchpage.js
+++ client/components/search/Searchpage.js
@@ -5,6 +5,23 @@ import Sidebar from '../sidebar/Sidebar';
  * Search page component
  */
 class SearchPage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchTerm: ''
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e){
+    this.setState({ [e.target.name]: e.target.value })
+  }
+  onSubmit(e){
+    e.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return(
       <div className="row">
@@ -15,13 +32,15 @@ class SearchPage extends Component {
           <div className="col s12 m12 l12">
             <div className="container">
               <h5 className="center-align uppercase" style={{ marginBottom: '2rem' }}>Search Users</h5>
-              <div className="row">
-                <div className="input-field col s12">
-                  <input placeholder="Enter a username" id="username" name="username" type="text" className="validate" style={{ display: 'inline-block', width: '80%', marginRight: '.3rem' }}/>
-                  <label htmlFor="username">Search</label>
-                  <a href="" className="btn waves-effect waves-light">Search</a>
+              <form action="" onSubmit={ this.onSubmit }>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input placeholder="Enter a username" id="searchTerm" name="searchTerm" type="text" value={ this.state.searchTerm } onChange={ this.onChange } className="validate" style={{ display: 'inline-block', width: '80%', marginRight: '.3rem' }}/>
+                    <label htmlFor="searchTerm">Search</label>
+                    <a href="" className="btn waves-effect waves-light">Search</a>
+                  </div>
                 </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
