05e7980fef510c285b4edcc5cb4a077b656d0dbc
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-06 18:21:33 +0100

    UI fix on search user page

--- client/components/search/Searchpage.js
+++ client/components/search/Searchpage.js
@@ -59,16 +59,16 @@ class SearchPage extends Component {
           <div className="card-panel hoverable">
             <div className="row">
               <div className="col s12">
-                <span className="bold">Username:</span><span className="bold">@{ user.username }</span>
+                <span className="bold">Username:</span><span className="bold margin-h">@{ user.username }</span>
               </div>
               <div className="col s6">
-                <span className="bold">Email:</span><span className="italics">{ user.email }</span>
+                <span className="bold">Email:</span><span className="italics margin-h">{ user.email }</span>
               </div>
               <div className="col s6">
-                <span className="bold">Groups:</span><span className="italics">{ user.Groups.length }</span>
+                <span className="bold">Groups:</span><span className="italics margin-h">{ user.Groups.length }</span>
               </div>
               <div className="col s12">
-                <span className="bold">Phone:</span><span className="italics">{ user.phone }</span>
+                <span className="bold">Phone:</span><span className="italics margin-h">{ user.phone }</span>
               </div>
             </div>
           </div>
