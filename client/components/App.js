1b01a7c4089135295f298b2f25f2bfe29118682d
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-06 19:44:57 +0100

    Add message page component route

--- client/components/App.js
+++ client/components/App.js
@@ -4,6 +4,7 @@ import HomePage from './home/HomePage';
 import DashboardPage from './dashboard/DashboardPage'
 import GroupPage from './group/GroupPage';
 import SearchPage from './search/Searchpage';
+import MessagePage from './message/MessagePage';
 import requireAuth from '../utilities/requireAuth';
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
           <Route exact path="/dashboard" component={ requireAuth(DashboardPage) } />
           <Route exact path="/group" component={ requireAuth(GroupPage) } />
           <Route exact path="/search" component={ requireAuth(SearchPage) } />
+          <Route exact path="/message" component={ requireAuth(MessagePage) } />
           <Route render={() => {
             return <p>404 Not Found!</p>;
           }} />
