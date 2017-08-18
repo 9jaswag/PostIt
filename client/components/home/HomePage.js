c58fe48c6054bb9acf397eab6f108094d905a85c
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-18 22:44:56 +0100

    es lint fixes

--- client/components/home/HomePage.js
+++ client/components/home/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import SignupForm from '../signup/SignupForm';
 import SigninForm from '../signin/SigninForm';
 import userSignupRequest from '../../actions/signupActions';
