665e078460859006e646bd7583a3312a3f88bc68
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-19 19:47:25 +0100

    add more group page test

--- client/__test__/components/GroupPage.test.js
+++ client/__test__/components/GroupPage.test.js
@@ -1,40 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { mount, shallow } from 'enzyme';
-import TestUtils from 'react-addons-test-utils';
+import { shallow } from 'enzyme';
+// import ShallowRenderer from 'react-test-renderer/shallow';
 import configureStore from 'redux-mock-store'
 import { GroupPage } from '../../components/group/GroupPage';
+import { Sidebar } from '../../components/sidebar/Sidebar';
 import mockSessionStorage from '../mocks/mockSessionStorage';
 
 Object.defineProperty(window, 'sessionStorage', { value: mockSessionStorage });
 
 
 describe('Group page Component', () => {
-  let mountedComponent;
-  let props;
-  const groupPage = () => {
-    if (!mountedComponent) {
-      mountedComponent = shallow(<GroupPage {...props}/>);
-    }
-    return mountedComponent;
-  }
-  beforeEach(() => {
-    const props = {
-      groupDetails: jest.fn(),
-      getMessages: jest.fn(),
-      passMessage: jest.fn(),
-      updateReadBy: jest.fn()
-    }
-  });
+  const props = {
+    groupDetails: '3 HNG',
+    getMessages: jest.fn(() => Promise.resolve()),
+    passMessage: jest.fn(),
+    updateReadBy: jest.fn()
+  };
+  // const renderer = new ShallowRenderer();
   it('should render without crashing', () => {
-    const component = groupPage();
+    const component = shallow(<GroupPage {...props}/>);
     expect(component.node.type).toEqual('div');
   });
-  // it('sessionStorage should contain groupDetails', () => {
-  //   const component = groupPage();
-  //   mockSessionStorage.setItem()
-  //   console.log('===========> component', component);
-  //   console.log('===========> session', sessionStorage);
-  // });
+  it('should contain the method componentDidMount', () => {
+    const component = shallow(<GroupPage {...props}/>);
+    const componentDidMountSpy = jest.spyOn(component.instance(), 'componentDidMount');
+    component.instance().componentDidMount();
+    expect(componentDidMountSpy).toHaveBeenCalledTimes(1);
+  });
+  it('should contain the method filterMessages', () => {
+    const messages = [{
+      "id": 3,
+      "title": "message",
+      "message": "body",
+      "priority": "normal",
+      "author": "chuks",
+      "readby": "chuks",
+      "groupId": 3,
+      "userId": 1
+    }];
+    const component = shallow(<GroupPage {...props}/>);
+    const filterMessages = jest.spyOn(component.instance(), 'filterMessages');
+    component.instance().filterMessages(messages);
+    expect(filterMessages).toHaveBeenCalledTimes(1);
+  });
 });
