e25cf60c442582af032e78422ee84844487a3eb0
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-19 19:47:08 +0100

    modify dashboard page test

--- client/__test__/components/DashboardPage.test.jsx
+++ client/__test__/components/DashboardPage.test.jsx
@@ -11,27 +11,22 @@ Object.defineProperty(window, 'sessionStorage', { value: mockSessionStorage });
 
 
 describe('Dashboard page Component', () => {
-  let mountedComponent;
-  let props;
-  const dashboardPage = () => {
-    if (!mountedComponent) {
-      mountedComponent = shallow(<DashboardPage {...props}/>);
-    }
-    return mountedComponent;
-  }
-  beforeEach(() => {
+  it('should render without crashing', () => {
     const props = {
       getGroups: jest.fn(),
       setGroupId: jest.fn(),
       getMessages: jest.fn()
     }
-  });
-  it('should render without crashing', () => {
-    const component = dashboardPage();
+    const component = shallow(<DashboardPage {...props}/>);
     expect(component.node.type).toEqual('div');
   });
   it('sessionStorage should contain groupDetails', () => {
-    const component = dashboardPage();
+    const props = {
+      getGroups: jest.fn(),
+      setGroupId: jest.fn(),
+      getMessages: jest.fn()
+    }
+    const component = shallow(<DashboardPage {...props}/>);
     // mockSessionStorage.setItem()
     // console.log('===========> component', component);
     // console.log('===========> session', sessionStorage);
