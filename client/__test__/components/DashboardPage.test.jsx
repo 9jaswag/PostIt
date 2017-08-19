9a3298369ddc1b109774a3786d9b38020e2aa0d5
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-19 15:18:29 +0100

    modify dashboard page test

--- client/__test__/components/DashboardPage.test.jsx
+++ client/__test__/components/DashboardPage.test.jsx
@@ -5,16 +5,35 @@ import { mount, shallow } from 'enzyme';
 import TestUtils from 'react-addons-test-utils';
 import configureStore from 'redux-mock-store'
 import { DashboardPage } from '../../components/dashboard/DashboardPage';
+import mockSessionStorage from '../mocks/mockSessionStorage';
+
+Object.defineProperty(window, 'sessionStorage', { value: mockSessionStorage });
 
 
-describe('Homepage Component', () => {
-  it('should render without crashing', () => {
+describe('Dashboard page Component', () => {
+  let mountedComponent;
+  let props;
+  const dashboardPage = () => {
+    if (!mountedComponent) {
+      mountedComponent = shallow(<DashboardPage {...props}/>);
+    }
+    return mountedComponent;
+  }
+  beforeEach(() => {
     const props = {
       getGroups: jest.fn(),
       setGroupId: jest.fn(),
       getMessages: jest.fn()
     }
-    const component = shallow(<DashboardPage {...props}/>);
+  });
+  it('should render without crashing', () => {
+    const component = dashboardPage();
     expect(component.node.type).toEqual('div');
+  });
+  it('sessionStorage should contain groupDetails', () => {
+    const component = dashboardPage();
+    // mockSessionStorage.setItem()
+    // console.log('===========> component', component);
+    // console.log('===========> session', sessionStorage);
   });
 });
