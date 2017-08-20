803ea029506e6f1f216d897967f7563d21947669
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-20 20:31:17 +0100

    modify signup action test

--- client/__test__/actions/signupActions.test.jsx
+++ client/__test__/actions/signupActions.test.jsx
@@ -1,41 +1,37 @@
-// import configureMockStore from 'redux-mock-store';
-// import thunk from 'redux-thunk';
-// import nock from 'nock';
-// import { mount, shallow } from 'enzyme';
-// import userSignupRequest from '../../actions/signupActions';
-// import { SET_CURRENT_USER } from '../../actions/types';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import expect from 'expect';
+import userSignupRequest from '../../actions/signupActions';
+import { SET_CURRENT_USER } from '../../actions/types';
+import mockLocalStorage from '../../__mocks__/mockLocalStorage';
 
-// const middlewares = [thunk];
-// const mockStore = configureMockStore(middlewares);
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+window.localStorage = mockLocalStorage;
 
-// describe('Sign up a user', () => {
-//   afterEach(() => {
-//     nock.cleanAll();
-//   });
+describe('Sign up a user', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
 
-//   it('it does something', () => {
-//     const userData = {
-//       username: 'chukss',
-//       email: 'chuks.opia@andela.comm',
-//       password: 'chukspass',
-//       phone: '2347033130440'
-//     };
-//     nock('http://localhost:9000/')
-//       .post('/api/user/signup', userData)
-//       .reply(201, { body: {
-//         success: true,
-//         message: 'Sign up succesful.',
-//         data: {
-//           token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjcsInVzZXJFbWFpbCI6ImNodWtzLm9waWFAYW5kZWxhLmNvbW0iLCJ1c2VyVXNlcm5hbWUiOiJjaHVrc3MiLCJ1c2VyUGhvbmUiOiIyMzQ3MDMzMTMwNDQwIiwiaWF0IjoxNTAzMDc1MDgwLCJleHAiOjE1MDMxNjE0ODB9.0SX6NVMqqQpgdUebW3iRBJz8oerTtfzYUm4ADESM7fk'
-//         }
-//       } });
-//     const expectedActions = [
-//       { type: SET_CURRENT_USER },
-//     ];
-//     const store = mockStore();
-//     return store.dispatch(userSignupRequest()).then(() => {
-//       // return of async actions
-//       expect(store.getActions()).toEqual(expectedActions);
-//     });
-//   });
-// });
+  it('creates an action SET_CURRENT_USER on successful user update', () => {
+    // nock('http://localhost:9000/')
+    //   .post('/api/user/signup')
+    //   .reply(200, { body: {
+    //     success: true,
+    //     message: 'Sign up succesful.',
+    //     data: {
+    //       token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjcsInVzZXJFbWFpbCI6ImNodWtzLm9waWFAYW5kZWxhLmNvbW0iLCJ1c2VyVXNlcm5hbWUiOiJjaHVrc3MiLCJ1c2VyUGhvbmUiOiIyMzQ3MDMzMTMwNDQwIiwiaWF0IjoxNTAzMDc1MDgwLCJleHAiOjE1MDMxNjE0ODB9.0SX6NVMqqQpgdUebW3iRBJz8oerTtfzYUm4ADESM7fk'
+    //     }
+    //   } });
+    // const expectedActions = [
+    //   { type: SET_CURRENT_USER }
+    // ];
+    // const store = mockStore({ users: {} });
+    // return store.dispatch(userSignupRequest()).then(() => {
+    //   // return of async actions
+    //   expect(store.getActions()).toEqual(expectedActions);
+    // });
+  });
+});
