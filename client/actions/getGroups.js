42c46dc1b3f16e26e1154f133d3fafac89cef57a
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-22 21:23:13 +0100

    modify get groups actions

--- client/actions/getGroups.js
+++ client/actions/getGroups.js
@@ -3,12 +3,22 @@
  */
 
 import axios from 'axios';
+import { GET_USER_GROUPS } from './types';
+
+export const setUserGroups = (groups) => {
+  return {
+    type: GET_USER_GROUPS,
+    groups
+  };
+};
 
 /**
  * @return {promise} returns an array of groups
  * @param {object} data contains details of groups to be fetched
  */
 const getGroups = data =>
-  (dispatch) => axios.get('/api/users/one');
+  dispatch => axios.get('/api/users/one').then((res) => {
+    dispatch(setUserGroups(res.data.data.Groups));
+  });
 
 export default getGroups;
