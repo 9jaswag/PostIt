df2bc4a4edd1b11f2025e75a929d0a3a4265fa3b
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-24 17:20:35 +0100

    remove multiple mapping

--- client/components/dashboard/DashboardPage.js
+++ client/components/dashboard/DashboardPage.js
@@ -8,54 +8,24 @@ import getMessages from '../../actions/getMessages';
 export class DashboardPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      groups: []
-    }
-    this.onLoad = this.onLoad.bind(this);
     this.onClick = this.onClick.bind(this);
   }
-
-  onLoad() {
-    const getGroup = new Promise ((resolve) => {
-      resolve(this.props.getGroups());
-    });
-    getGroup.then(() => {
-      const groupsWithNotification = [];
-      this.props.groups.map(group => {
-        this.props.getMessages(group.id).then(
-          (res) => {
-            // map returned message array
-            let count = 0;
-            res.data.data.map(message => {
-              if (!message.readby.split(',').includes(this.props.user.userUsername)) {
-                count ++;
-              }
-            })
-            groupsWithNotification.push({id: group.id, name: group.name, count});
-            this.setState({ groups: groupsWithNotification })
-          },
-          (err) => {}
-        );
-      });
-    });
-  }
-
   onClick(e) {
     sessionStorage.setItem('groupDetails', e.target.dataset.id + ' ' + e.target.dataset.name );
   }
   componentDidMount() {
-    this.onLoad();
+    this.props.getGroups();
   }
 
-  render() {
-    const { groups } = this.state;
-    const groupCards = groups.map(group =>
-      <a onClick= { this.onClick } href="/group"  className="tooltipped pointer" data-position="right" data-delay="50" data-tooltip={ group.description }  key={group.id}>
+  render() {;
+    const groups = this.props.groups;
+    const groupCards = groups.map((group) => {
+      return <a className="pointer" href="/group" data-position="right" data-delay="50" data-tooltip={ group.group.description } key={group.group.id} onClick={ this.onClick }>
         <div className="col s12 m6 l4">
-          <div data-id={group.id} data-name={group.name} className="card-panel hoverable">{ group.name }{ (group.count > 0) ? <span className="new badge">{group.count}</span> : null}</div>
+          <div data-id={group.group.id} data-name={group.group.name} className="card-panel hoverable">{ group.group.name } { (group.unreadCount > 0) ? <span className="new badge">{group.unreadCount}</span> : null}</div>
         </div>
       </a>
-    );
+    });
     return(
       <div>
         <div className="row">
@@ -66,8 +36,7 @@ export class DashboardPage extends Component {
             <div className="col s12 m12 l12">
               <h5 className="center-align uppercase" style={{ marginBottom: '2rem' }}>My Groups</h5>
               { /*Group cards*/ }
-               { (groups.length > 0) ? groupCards : <h6 className="center-align margin-v2">No Groups Available. Create one from the left sidebar</h6> } 
-              {/* { groupCards } */}
+              { (groups.length > 0) ? groupCards : <h6 className="center-align margin-v2">No Groups Available. Create one from the left sidebar</h6> } 
             </div>
           </div>
         </div>
