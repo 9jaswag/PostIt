bbfd6e004cb669b34d3252d01ad5c594575ac801
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-08-26 15:50:35 +0100

    modify webpack config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,53 +1,56 @@
-const debug = process.env.NODE_ENV !== 'production';
+/**
+ * Webpack config
+ */
+
+// import webpack from 'webpack';
+// import path from 'path';
 const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  // devtool: debug ? 'inline-sourcemap' : true,
+  // debug: true,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   entry: './client/index.js',
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    // Eliminate duplicate plugins
+    new webpack.optimize.DedupePlugin(),
+    // Minify Js
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
   module: {
     loaders: [
+      { test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+        query: {
+          presets: ['es2015', 'stage-2']
+        }
+      },
       { test: /\.jsx$/,
         loader: 'babel-loader?sourceMap',
         query: {
           presets: ['es2015', 'react', 'stage-2']
         }
       },
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-2'],
-          plugins: ['react-html-attrs', 'transform-decorators-legacy',
-            'transform-class-properties'],
-        }
-      },
-      {
-        test: /\.scss$/,
-        loader: 'style-loader!css-loader!sass-loader'
-      },
-      { test: /\.css$/,
-        loaders: [
-          'style-loader',
-          'css-loader?importLoaders=1',
-          'font-loader?format[]=truetype&format[]=woff&format[]=embedded-opentype'
-        ] },
-      { test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file-loader?name=fonts/[name].[ext]'
-      }
+      { test: /\.scss$/, loader: 'style-loader!css-loader?url=false!sass-loader' },
+      { test: /(\.css)$/, loaders: ['style-loader', 'css-loader'] },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
     ]
   },
-  output: {
-    path: `${__dirname}/client/dist/`,
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  plugins: [
-    // Minify Js
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ],
   node: {
     net: 'empty',
     dns: 'empty'
