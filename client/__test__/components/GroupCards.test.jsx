259142de3974196423cb8fd4a73ea67b9c2b6405
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-09-01 16:45:36 +0100

    add more tests

---
+++ client/__test__/components/GroupCards.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import GroupCards from '../../components/group/GroupCards';
+
+describe('Create group modal', () => {
+  const props = {
+    onClick: jest.fn(),
+    group: {
+      group: {
+        "id": 9,
+        "name": "true love",
+        "description": 'a group description'
+      }
+    }
+  }
+  it('should render without crashing', () => {
+    const component = shallow(<GroupCards onClick={ props.onClick } group={ props.group } />);
+    expect(component.node.type).toEqual('div');
+  });
+});
