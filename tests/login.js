3a3d3f7d8a4579679ddf789e6440763da0f4f768
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-09-04 08:08:58 +0100

    fix nightwatch

--- tests/login.js
+++ tests/login.js
@@ -1,15 +1,30 @@
 module.exports = {
-  'user sign up without credentials': (browser) => {
-    browser
+  'user sign up without credentials': (client) => {
+    client
       .url('http://localhost:9000/')
       .waitForElementVisible('body', 5000)
-      .setValue('input[name=username]', '')
-      .setValue('input[name=password]', '')
-      .setValue('input[name=email]', '')
-      .setValue('input[name=phone]', '')
-      .click('.signup')
+      .click('a.waves-effect.waves-light.btn.modal-trigger.signin-modal')
+      .setValue('input#username', '')
+      .setValue('input#password', '')
+      .setValue('input#email', '')
+      .setValue('input#phone', '')
+      .click('input.btn.signup')
       .pause(2000)
-      .assert.urlEquals('http://localhost:9000');
-    browser.end();
+      .assert.urlEquals('http://localhost:9000/#signupModal');
+    client.end();
+  },
+  'user sign up successful': (client) => {
+    client
+      .url('http://localhost:9000/')
+      .waitForElementVisible('body', 5000)
+      .click('a.waves-effect.waves-light.btn.modal-trigger.signin-modal')
+      .setValue('input#username', 'chioma')
+      .setValue('input#password', 'password')
+      .setValue('input#email', 'chioma@email.com')
+      .setValue('input#phone', '07077123456')
+      .click('input.btn.signup')
+      .pause(2000)
+      .assert.urlEquals('http://localhost:9000/dashboard');
+    client.end();
   }
 };
