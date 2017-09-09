b8c463eb8041ef71dc4c38863ccae0c4f26d1c10
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-09-09 14:17:41 +0100

    add helper test

--- server/test/tests.js
+++ server/test/tests.js
@@ -4,6 +4,7 @@ import chaiHttp from 'chai-http';
 import chai from 'chai';
 import app from '../app';
 import models from '../models';
+import customSort from '../../helpers/customSort';
 
 process.env.NODE_ENV = 'test';
 const should = chai.should();
@@ -826,6 +827,62 @@ describe('PostIT API Tests:', () => {
           res.body.removedUser.should.equals(1);
           done();
         });
+    });
+  });
+  describe('API route for getting group member count', () => {
+    it('returns error if no token is provided', (done) => {
+      chai.request(app)
+        .get('/api/v1/group/1/count')
+        .type('form')
+        .send({
+          userId: 1,
+        })
+        .end((err, res) => {
+          res.should.have.status(403);
+          res.body.message.should.equals(
+            'User not authenticated. Failed to authenticate token.');
+          done();
+        });
+    });
+    it('returns the member count of a group', (done) => {
+      chai.request(app)
+        .get('/api/v1/group/1/count')
+        .type('form')
+        .set('x-access-token', token)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.data.should.equals(2);
+          done();
+        });
+    });
+  });
+  describe('customSort helper function', () => {
+    it('should be a function', (done) => {
+      customSort.should.be.a('function');
+      done();
+    });
+    it('should return an array sorted by ID', (done) => {
+      const array = [
+        {
+          group: {
+            id: 13,
+            name: 'Andela',
+            description: 'an Andelan group'
+          },
+          unreadCount: 1
+        },
+        {
+          group: {
+            id: 2,
+            name: 'PostIT',
+            description: "A group for HNG's Factory product"
+          },
+          unreadCount: 4
+        }
+      ];
+      const sortedArr = array.sort(customSort);
+      sortedArr[0].group.id.should.equal(2);
+      done();
     });
   });
 });
